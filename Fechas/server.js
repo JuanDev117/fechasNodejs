19bd12b817276108503e4d7c15dcf929f27d5401
jeremias <juansebas7011@gmail.com>
2026-02-15 21:57:11 -0500

    he creado un servidor

---
+++ Fechas/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+
+const server = http.createServer((req,res)=> {
+
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    
+    res.end('Hola Mundo desde Node.js');
+});
+
+
+server.listen(3000, "127.0.0.1", () => {
+
+    console.log("servver runnig...");
+});
